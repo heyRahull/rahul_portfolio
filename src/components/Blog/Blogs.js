35eaf34515eb48506c55e651764691713c8b9e7e
Rahul Agarwal <rahul123agarwal99@gmail.com>
2023-07-21 08:11:46 +0530

    cardData.js added

--- src/components/Blog/Blogs.js
+++ src/components/Blog/Blogs.js
@@ -33,21 +33,7 @@ const Blogs = () => {
                 name={card.name}
               />
             ))}
-            {/* <Card
-              excerpt="Deploying a React.js app to Netlify using GitHub provides a seamless workflow for hosting and managing your web application. In this guide, we'll walk through the steps to deploy your React.js app to Netlify, taking advantage of the powerful integration with GitHub. "
-              image="https://i.ytimg.com/vi/B5zka8akZBU/maxresdefault.jpg"
-              link="https://rahul123agarwal99.medium.com/how-to-deploy-react-js-app-to-netlify-using-github-2023-7057dc258ca3"
-              name="Host ReactJs Blog"
-            /> */}
-            {/* <Card
-              excerpt="Vanilla Js or Plain Js or Javascript in Browser. We might get a
-              thought that why learn vanilla JavaScript when you’ve got Angular,
-              React and Node.js? Well, there are several reasons for choosing
-              the vanilla js to learn..."
-              image={vanillaJs}
-              link="https://rahulagarwal.hashnode.dev/vanilla-js-or-plain-js-or-javascript-in-browser"
-              name="vanilla JS"
-            /> */}
+
             <Card
               excerpt="React Js is a library of Javascript. As a beginner it
               sometimes gets too tough to select from various content that
