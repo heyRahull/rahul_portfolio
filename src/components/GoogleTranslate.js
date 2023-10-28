fd9f36b068764bd0e5e41c007dc8d5d684357953
Rahul Agarwal <rahul123agarwal99@gmail.com>
2023-10-28 22:40:21 +0530

    google translate logo added

---
+++ src/components/GoogleTranslate.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class GoogleTranslate extends Component {
+  render() {
+    return (
+      <>
+        <div
+          id="google_translate_element"
+          class="splash_page_translation_desktop"
+        ></div>
+      </>
+    );
+  }
+}
+
+export default GoogleTranslate;
